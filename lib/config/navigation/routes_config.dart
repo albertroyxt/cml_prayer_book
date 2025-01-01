d7125d9bc1299e774113eef3b7b68e8d1fcbc651
Albert Roy <albert.roy@maisondauraine.com>
2025-01-02 00:40:18 +0530

    Router

--- lib/config/navigation/routes_config.dart
+++ lib/config/navigation/routes_config.dart
@@ -1,4 +1,15 @@
 class Routes{
   static const root = '/';
+  static const homeScreen = '/homeScreen';
 
 }
+
+void configureRoutes(FluroRouter router) {
+  router.define(Routes.homeScreen, handler:  _homeHandler);
+}
+
+var _homeHandler = Handler(
+  handlerFunc: (context, params) {
+    return HomeScreen();
+  },
+);
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'modules/splash/presentation/screens/app_wrapper.dart';
 void main()  async {
    WidgetsFlutterBinding.ensureInitialized();
    runApp(const AppWrapper());
+   final router = FluroRouter();
+   configureRoutes(router);
 }
 
 
