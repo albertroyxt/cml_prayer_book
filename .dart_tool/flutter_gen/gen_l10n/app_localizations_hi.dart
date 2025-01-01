fc1848e21027f108934c14b555e1fc5bd4159755
Albert Roy <albert.roy@maisondauraine.com>
2025-01-02 00:32:22 +0530

    update
    update

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_hi.dart
@@ -0,0 +1,11 @@
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Hindi (`hi`).
+class AppLocalizationsHi extends AppLocalizations {
+  AppLocalizationsHi([String locale = 'hi']) : super(locale);
+
+  @override
+  String get helloWorld => 'Thank God!! Namasthe';
+}
