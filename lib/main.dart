18e5947ccc1bb96a124064c15164fb29f6a4ed20
Albert Roy <albert.roy@maisondauraine.com>
2025-01-01 23:59:44 +0530

    Initial Commit
    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'modules/home/presentation/screens/home_screen.dart';
+import 'modules/splash/presentation/screens/app_wrapper.dart';
+
+void main()  async {
+   WidgetsFlutterBinding.ensureInitialized();
+   runApp(const AppWrapper());
+}
+
+
+
+
+
+
